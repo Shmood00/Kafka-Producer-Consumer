2a850e73719b2b2b3e418fd9c5abd821a774eac5
shmood00 <43577426+Shmood00@users.noreply.github.com>
2021-01-31 20:32:14 -0500

    Fixed error in install script

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ if [ ! -e $CONFIG_FILE ]; then
     echo "Creating your 'config.ini' file."
     echo ""
 
-    echo -e "[DEFAULT]\nhost=$host\ntopic=$topic\nssl_cafile=/aiven-kafka/certs/ca.pem\nssl_certfile=/aiven-kafka/certs/client.cert\nssl_keyfile=/aiven-kafka/certs/client.key\n\n[PostgreSQL]\ndbusername=$dbusername\ndbpassword=$dbpassword\ndbhost=$dbhost\ndbport=$dbport\ndbname=$dbname" >> $CONTAINER_ALREADY_STARTED
+    echo -e "[DEFAULT]\nhost=$host\ntopic=$topic\nssl_cafile=/aiven-kafka/certs/ca.pem\nssl_certfile=/aiven-kafka/certs/client.cert\nssl_keyfile=/aiven-kafka/certs/client.key\n\n[PostgreSQL]\ndbusername=$dbusername\ndbpassword=$dbpassword\ndbhost=$dbhost\ndbport=$dbport\ndbname=$dbname" >> $CONFIG_FILE
     echo ""
     echo "Your 'config.ini' file has been created and moved to the location: $CONFIG_FILE"
     echo "Please navigate to the aiven-kafka/ folder to run the program."
