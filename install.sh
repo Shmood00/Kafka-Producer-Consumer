ced6bb7bbf19a903b699b45c263b7d1a3f96ef75
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2021-01-31 22:00:22 -0500

    minor format changes

--- install.sh
+++ install.sh
@@ -50,9 +50,9 @@ if [ ! -e $CONFIG_FILE ]; then
     echo ""
     echo "**********************************************************************************"
     echo "Creating your 'config.ini' file."
-    echo ""
-
+   
     echo -e "[DEFAULT]\nhost=$host\ntopic=$topic\nclient_id=$client_id\ngroup_id=$group_id\nssl_cafile=/aiven-kafka/certs/ca.pem\nssl_certfile=/aiven-kafka/certs/client.cert\nssl_keyfile=/aiven-kafka/certs/client.key\n\n[PostgreSQL]\ndbusername=$dbusername\ndbpassword=$dbpassword\ndbhost=$dbhost\ndbport=$dbport\ndbname=$dbname" >> $CONFIG_FILE
+    echo "***********************************************************************************"
     echo ""
     echo "***********************************************************************************"
     echo "Your 'config.ini' file has been created and moved to the location: $CONFIG_FILE"
